2ace83397f3c59414bb4f10aac6d0c2a7a66a3a9
punnreay <punnreayruos@gmail.com>
2022-05-11 16:16:11 +0700

    test 23

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         int punnreayy;
         String name = "punnreay";
         String neww = " test";
-        String new2 = " test2";
+        String new2 = " test23";
     }
 }
