919d9a87111ac1a9011d69d2e2b29f998adfd888
punnreay <punnreayruos@gmail.com>
2022-05-11 16:08:54 +0700

    new commit

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         int punnreayy;
         String name = "punnreay";
+        String neww = " test";
     }
 }
